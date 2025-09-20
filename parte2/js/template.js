ed91e02fe27b5878bcce781204699f0241ed4af0
Kelly jacome <Kellyjacome987@gmail.com>
2025-09-20 06:59:57 -0600

    componentes parte 2

---
+++ parte2/js/template.js
@@ -0,0 +1,25 @@
+ const template = document.getElementById("card-template");
+  const contenedor = document.getElementById("contenedor");
+  const imagen = document.getElementById("miImagen");
+
+
+  const productos = [
+    {titulo: "Laptop", desc: "16GB RAM", price : "10000000", image: "imagenes/pc.jpg"},
+    {titulo: "Mouse", desc: "Inalámbrico", price : 10000000, image:"imagenes/mouse.jpg"},
+    {titulo: "Audifonos", desc: "Inalámbrico", price : 10000000,  image:"imagenes/AudifonosAirpodsMAXInEarP903.webp"},
+    {titulo: "Celular", desc: "Inalámbrico", price : 10000000,image:"imagenes/celular.png"},
+    {titulo: "Tablet", desc: "Inalámbrico", price : 10000000,image:"imagenes/tablet.webp"},
+    
+
+  ];
+
+  productos.forEach(producto => {
+    const clon = template.content.cloneNode(true);
+    clon.querySelector(".title").textContent = producto.titulo;
+    clon.querySelector(".desc").textContent = producto.desc;
+    clon.querySelector(".price").textContent = producto.price;
+    clon.querySelector("img").src = producto.image;
+    
+    
+    contenedor.appendChild(clon);
+  });
